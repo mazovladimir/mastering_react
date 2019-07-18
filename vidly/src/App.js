fe218fab92cdfe26ccbc8d9b8b7074da1e6b1760
Vladimir Mazo <vladimir.mazo@icl-services.com>
2019-07-18 14:21:01 +0300

    Finished forms

--- vidly/src/App.js
+++ vidly/src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from 'react';
-import './App.css';
-import Movies from './components/movies'
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import "./App.css";
+import Movies from "./components/movies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm"
 
 class App extends Component {
   render() {
     return (
-      <main className="container" >
-        <Movies />
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect exact from="/" to="/movies" />
+            <Redirect to="not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
